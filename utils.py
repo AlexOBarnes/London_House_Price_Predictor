fcf52fc9c1d8472a4e77503c25c7418a1daabc16
Alex Barnes <alex.barnes@vitruvianpartners.com>
2025-03-17 17:51:20 +0000

    Created latitude longitude function

--- utils.py
+++ utils.py
@@ -1,1 +1,19 @@
 '''Functions that process requests from streamlit app'''
+from os import environ as ENV
+from dotenv import load_dotenv
+from opencage.geocoder import OpenCageGeocode
+import streamlit as st
+
+
+@st.cache_data
+def get_lat_lon(postcode):
+    geocoder = OpenCageGeocode(ENV['MAPS_API_KEY'])
+    result = geocoder.geocode(postcode)
+    if result:
+        return result[0]['geometry']['lat'], result[0]['geometry']['lng']
+    return None
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    print(get_lat_lon('SW1A 1AA'))
